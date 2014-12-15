bf8f8a587e71f852fb126335495803fe7b4c5a5e
JuneA <anna.czerwiec@gmail.com>
2014-12-15 14:12:06 +0000

    Changed steam/return URL. Temporary solution.

--- routes/auth.js
+++ routes/auth.js
@@ -22,8 +22,8 @@ passport.deserializeUser(function(obj, done) {
 
 
 passport.use(new SteamStrategy({
-    returnURL:'http://localhost:3000/auth/steam/return',
-    realm: 'http://localhost:3000/',
+    returnURL:'https://steamshuffle.herokuapp.com/steam/return',
+    realm: 'hhttps://steamshuffle.herokuapp.com/',
     apiKey: process.env.API_KEY
     },
     function(identifier, profile, done){
